0adb885026a7436e27da22ac7f091f12f65dbcc5
Charlie Hansford-White <charlie@charliehw.com>
2013-05-17 17:51:24 +0100

    Refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,98 +1,194 @@
 $(function () {
 
-	var $cf = $('#connect-four'),
-		$cols = $('.col', $cf),
-		translations = [
-			{x: 1, y: 0}, // Right
-			{x: 0, y: 1}, // Up
-			{x: 1, y: 1}, // Diagonal up right
-			{x: 1, y: -1} // Diagonal down right
-		],
-		grid = [ // The base grid where -1 is an empty cell
-			[-1,-1,-1,-1,-1,-1,-1],
-			[-1,-1,-1,-1,-1,-1,-1],
-			[-1,-1,-1,-1,-1,-1,-1],
-			[-1,-1,-1,-1,-1,-1,-1],
-			[-1,-1,-1,-1,-1,-1,-1],
-			[-1,-1,-1,-1,-1,-1,-1]
-		]
-		player = 0;
+	new cf.Game();
 
-	function victoryCheck(grid, y, x, id) {
-		var count, i, j;
+});
+
+
+var cf = {};
+
+cf.Game = function () {
+
+	this.gameState = new cf.GameState();
+	this.ai = new cf.AI(this);
+	this.player = 0;
+	this.activePlayer = 0;
+	this.complete = false; // Set to true when a victory is attained
+
+	this.$container = $('#connect-four');
+	this.$cols = $('.col', this.$container);
+	this.$cells = $('.cell', this.$container);
+
+	this.init();
+
+};
+
+cf.Game.prototype = {
+
+	init: function () {
+		this._reset();
+		this._bind();
+	},
+
+	_bind: function () {
+		var self = this;
+		self.$cols.on('click', function () {
+			var colIndex = self.$cols.index($(this)),
+				result = self.gameState.addToColumn(colIndex, false, self.activePlayer),
+				victoryCells;
+			if (!result || self.complete) {
+				return;
+			}
+			self.$cols.eq(colIndex).find('.cell').eq(result.lastTurn.rowIndex).removeClass('red yellow').addClass((self.activePlayer === 0 ? 'disc yellow' : 'disc red'));
+			if (victoryCells = result.victoryCheck()) {
+				self.complete = true;
+				self._highlightCells(victoryCells);
+				window.setTimeout(function () {
+					self._reset();
+					new cf.Game();
+				}, 2000);
+			}
+			self.activePlayer = self.activePlayer === 0 ? 1 : 0;
+			if (self.activePlayer === 1) {
+				self.ai.findTurn();
+			}
+		});
+	},
+
+	_reset: function () {
+		this.$cells.removeClass('disc highlight');
+		this.$cols.off('click');
+	},
+
+	_highlightCells: function (cells) {
+		var i;
+		for (i = 0; i < cells.length; i++) {
+			this.$cols.eq(cells[i].colIndex).find('.cell').eq(cells[i].rowIndex).addClass('highlight');
+		}
+	}
+
+};
+
+cf.GameState = function () {
+
+	this.grid = [ // The base grid where -1 is an empty cell
+		[-1,-1,-1,-1,-1,-1,-1],
+		[-1,-1,-1,-1,-1,-1,-1],
+		[-1,-1,-1,-1,-1,-1,-1],
+		[-1,-1,-1,-1,-1,-1,-1],
+		[-1,-1,-1,-1,-1,-1,-1],
+		[-1,-1,-1,-1,-1,-1,-1]
+	];
+
+};
+
+cf.GameState.prototype = {
+
+	constructor: cf.GameState,
+
+	addToColumn: function (colIndex, preserve, id) {
+		var tempState, i;
+		if (preserve) {
+			tempState = this.clone();
+		} else {
+			tempState = this;
+		}
+		for (i = tempState.grid.length - 1; i >= 0; i--) {
+			if (tempState.grid[i][colIndex] === -1) {
+				tempState.grid[i][colIndex] = id || 0;
+				tempState.lastTurn = {
+					rowIndex: i,
+					colIndex: colIndex,
+					player: id
+				};
+				return tempState;
+			}
+		}
+		// Column is full already
+		return false;
+	},
+
+	victoryCheck: function () {
+		var cells, i, j;
 		// Loop over each of the 4 directions
-		for (i = 0, l = translations.length; i < l; i++) {
-			count = 0;
+		for (i = 0, l = this.translations.length; i < l; i++) {
+			cells = [];
 			// Check along the line in that particular direction
 			for (j = -5; j < 5; j++) {
-				if (grid[y + (translations[i].y * j)] !== undefined) {
-					if (grid[y + translations[i].y * j][x + translations[i].x * j] === id) {
-						count++;
+				if (this.grid[this.lastTurn.rowIndex + (this.translations[i].y * j)] !== undefined) {
+					if (this.grid[this.lastTurn.rowIndex + this.translations[i].y * j][this.lastTurn.colIndex + this.translations[i].x * j] === this.lastTurn.player) {
+						cells.push({colIndex: this.lastTurn.colIndex + this.translations[i].x * j, rowIndex: this.lastTurn.rowIndex + this.translations[i].y * j});
 					} else {
 						// Maybe the line broke
-						count = 0;
+						cells = [];
 					}
 				}
-				if (count >= 4) {
-					return true;
+				if (cells.length >= 4) {
+					return cells;
 				}
 			}
 		}
-	}
+	},
 
-	function addToColumn(grid, colIndex, id) {
-		var tempGrid = deepCopy(grid), i,
-			added = false;
-		for (i = tempGrid.length - 1; i >= 0; i--) {
-			if (tempGrid[i][colIndex] === -1) {
-				tempGrid[i][colIndex] = id;
-				added = true;
-				break;
-			}
-		}
-		if (added) {
-			return {
-				grid: tempGrid,
-				rowIndex: i
-			};
-		} else {
-			// Column is full already
-			return false;
-		}
-	}
+	clone: function () {
+		var result = new cf.GameState();
+		result.grid = cf.utils.deepCopy(this.grid);
+		return result;
+	},
 
-	function doTurn(grid) {
+	translations: [ // Static final
+		{x: 1, y: 0}, // Right
+		{x: 0, y: 1}, // Up
+		{x: 1, y: 1}, // Diagonal up right
+		{x: 1, y: -1} // Diagonal down right
+	]
+
+};
+
+cf.Player = function (game, id) {
+	this.game = game;
+	this.id = id;
+};
+
+cf.AI = function (game) {
+	this.game = game;
+};
+
+cf.AI.prototype = {
+
+	findTurn: function () {
 		var scores = [0, 0, 0, 0, 0, 0, 0], i, colIndex;
-		scores = aiTurn(grid, 4, player, scores, true);
-		console.log(scores);
-		for (i = 0; i < scores.length; i++) {
-			if (colIndex === undefined || scores[i] > scores[colIndex]) {
+		scores = this.checkTurn(this.game.gameState, 4, this.game.activePlayer, scores, true);
+		for (i = 0; i < scores.length; i++) { // Find the highest score for a column that isn't full
+			if ((colIndex === undefined || scores[i] > scores[colIndex]) && this.game.gameState.addToColumn(i, true)) {
 				colIndex = i;
 			}
 		}
-		$cols.eq(colIndex).click();
-	}
+		this.game.$cols.eq(colIndex).click();
+	},
 
-	function aiTurn(grid, count, id, scores, initial, scoreTracker) {
-		var result, i = 0, counter, p;
+	checkTurn: function (state, count, id, scores, initial, scoreTracker) {
+		var result, i = 0, counter, p, victoryFound;
 		while (i < 7) {
 			p = id;
+			victoryFound = false;
 			if (initial) {
 				scoreTracker = i;
 			}
 			counter = count;
-			result = addToColumn(grid, i, p);
+			result = state.addToColumn(i, true, p);
 			if (result) {
-				if (victoryCheck(result.grid, result.rowIndex, i, p)) {
-					if (p === player) {
-						scores[scoreTracker] += counter;
+				if (result.victoryCheck()) {
+					if (p === this.game.activePlayer) {
+						victoryFound = true;
+						scores[scoreTracker] += Math.pow(counter, 3);
 					} else {
-						scores[scoreTracker] -= counter + 1;
+						scores[scoreTracker] -= Math.pow(counter, 3);
 					}
 				}
 				p = p === 0 ? 1 : 0;
-				if (counter-- > 0) {
-					aiTurn(result.grid, counter, p, scores, false, scoreTracker);
+				if (counter-- > 0 && !victoryFound) {
+					this.checkTurn(result, counter, p, scores, false, scoreTracker);
 				}
 			}
 			i += 1;
@@ -100,12 +196,11 @@ $(function () {
 		return scores;
 	}
 
+};
 
-	function victory() {
-		alert('Victory!');
-	}
+cf.utils = {
 
-	function deepCopy(obj) {
+	deepCopy: function (obj) {
 		if (Object.prototype.toString.call(obj) === '[object Array]') {
 			var out = [], i = 0, len = obj.length;
 			for ( ; i < len; i++ ) {
@@ -123,55 +218,5 @@ $(function () {
 		return obj;
 	}
 
-
-	$cols.click(function () {
-		var colIndex = $cols.index($(this)),
-			$cells = $cols.eq(colIndex).children(),
-			$target = $cells.not('.disc').last(),
-			rowIndex = $cells.index($target);
-		if (rowIndex === -1) {
-			return; // Column is full, go again
-		}
-		grid[rowIndex][colIndex] = player; // Set the matric value to the player id
-		$target.addClass((player === 0 ? 'disc yellow' : 'disc red'));
-		if (victoryCheck(grid, rowIndex, colIndex, player)) {
-			victory();
-		}
-		player = player === 0 ? 1 : 0;
-		if (player === 1) {
-			doTurn(grid);
-		}
-	});
-
-});
-
-var cf = {};
-
-cf.Game = function () {
-
-	this.$cf = $('#connect-four'),
-	this.$cols = $('.col', $cf),
-	this.translations = translations = [
-		{x: 1, y: 0}, // Right
-		{x: 0, y: 1}, // Up
-		{x: 1, y: 1}, // Diagonal up right
-		{x: 1, y: -1} // Diagonal down right
-	];
-	this.grid = [ // The base grid where -1 is an empty cell
-		[-1,-1,-1,-1,-1,-1,-1],
-		[-1,-1,-1,-1,-1,-1,-1],
-		[-1,-1,-1,-1,-1,-1,-1],
-		[-1,-1,-1,-1,-1,-1,-1],
-		[-1,-1,-1,-1,-1,-1,-1],
-		[-1,-1,-1,-1,-1,-1,-1]
-	];
-
 };
 
-cf.Game.prototype = {
-
-	_bind: function () {
-
-	}
-
-}
